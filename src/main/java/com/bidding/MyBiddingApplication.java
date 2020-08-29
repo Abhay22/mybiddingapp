bc0201c2aad9569691e55e98b3ecb966426f5f92
Abhay Pandit <abhay.pndt@gmail.com>
2020-08-29 22:32:40 +0530

    Initial Commit

---
+++ src/main/java/com/bidding/MyBiddingApplication.java
@@ -0,0 +1,13 @@
+package com.bidding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBiddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBiddingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bidding/MyBiddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBiddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
